642aff687f11982309dec09f34e3b7fed105c009
Julio Carvalho <jcarvalhok3@gmail.com>
2022-01-28 14:14:30 -0300

    método de update do Cliente

--- src/main/java/br/com/primeiroProjeto/demo/service/ClienteService.java
+++ src/main/java/br/com/primeiroProjeto/demo/service/ClienteService.java
@@ -1,8 +1,12 @@
 package br.com.primeiroProjeto.demo.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
+import br.com.primeiroProjeto.demo.exception.ResourceNotFoundException;
 import br.com.primeiroProjeto.demo.model.Cliente;
 import br.com.primeiroProjeto.demo.repository.ClienteRepository;
 
@@ -14,5 +18,12 @@ public class ClienteService {
 	
 	public Cliente create(Cliente c) {
 		return clienteRepository.save(c); 
+	}
+	
+	public Cliente update(Cliente c, String nome, String email, Long id) throws ResourceNotFoundException {
+		Cliente cliente = new Cliente();
+		Cliente found = clienteRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Cliente nao encontrado"));
+		
+		return clienteRepository.save(c);
 	}
 }
